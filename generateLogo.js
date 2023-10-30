5861422534f47b9c9ab17045f492d85f0319cb2f
OleJG <flucode9@gmail.com>
2023-10-30 19:33:04 -0400

    edits/add to SVG code

--- generateLogo.js
+++ generateLogo.js
@@ -19,3 +19,21 @@ const askColor = (label) => new Promise(resolve => {
 const askShape = () => new Promise(resolve => {
     rl.question('Choose a shape (circle, triangle, square): ', resolve);
 });
+
+const generateSVG = (text, textColor, shape, shapeColor) => {
+    let svgContent = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
+    switch (shape) {
+        case 'circle':
+            svgContent += `<circle cx="150" cy="100" r="90" fill="${shapeColor}" />`;
+            break;
+        case 'triangle':
+            svgContent += `<polygon points="150,10 280,190 20,190" fill="${shapeColor}" />`;
+            break;
+        case 'square':
+            svgContent += `<rect width="180" height="180" x="60" y="10" fill="${shapeColor}" />`;
+            break;
+    }
+    svgContent += `<text x="150" y="110" font-family="Arial" font-size="48" fill="${textColor}" text-anchor="middle">${text}</text>`;
+    svgContent += `</svg>`;
+    return svgContent;
+};
